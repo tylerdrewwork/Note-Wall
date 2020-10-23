5c6b014f82e2424c00c641bc5675da4905098e76
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-23 11:49:05 -0500

    added author id column to notes_db

--- models/note.js
+++ models/note.js
@@ -1,7 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
   var Note = sequelize.define("Note", {
     text: DataTypes.STRING,
-    views: DataTypes.INTEGER
+    views: DataTypes.INTEGER,
+    author_id: DataTypes.INTEGER
   });
   return Note;
 };
