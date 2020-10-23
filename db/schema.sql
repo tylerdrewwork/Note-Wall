c4cf944ad3abb31c998a36650757ca401f96ae06
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-22 20:53:46 -0500

    created schema file

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE  notes_db;
+USE notes_db;
+
+CREATE TABLE notes
+(
+  id int NOT NULL AUTO_INCREMENT,
+  author_id int,
+  text varchar(255) NOT NULL,
+  views int default 0,
+  PRIMARY KEY (id)
+);
