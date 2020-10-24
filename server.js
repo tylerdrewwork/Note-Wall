4f1a4afc67bdad6b2c0cae0f3a29cfabdefabbc9
JCJ <jesse.jackson.c@gmail.com>
2020-10-24 10:22:36 -0500

    Added dependency of html-routes within server file

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
