2fe45fb5783a52be014b0efdf14dac8253f670cb
JCJ <jesse.jackson.c@gmail.com>
2020-10-24 10:06:09 -0500

    Pathing set up for index route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,3 +3,12 @@
 // Dependencies
 
 let path = require("path");
+
+// Routes
+
+module.exports = function(app) {
+    // base index route
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/boilerplate.html"));
+    });
+};
