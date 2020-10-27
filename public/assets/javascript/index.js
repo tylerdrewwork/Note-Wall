26cf49ee8e7c5b121cb7976f1dfdadbe73640532
Sakiskid <tylerdrew.home@gmail.com>
2020-10-26 21:17:38 -0500

    basic animation implementation on note click

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -32,4 +32,12 @@ $(document).ready(function() {
 
 function openNote() {
     console.log("Opening note... ");
+
+    let element = $("#note-new-form");
+
+    anime({
+        targets: '#note-new-form',
+        translateY: -270,
+        opacity: 1,
+    })
 }
