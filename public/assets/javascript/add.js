e9637059a8cd70b7b6a85df747f700a8e48fb5d7
JCJ <jesse.jackson.c@gmail.com>
2020-10-26 21:12:06 -0500

    Added 'add.js' file to include on click event and establishing newNote object to send through post request

---
+++ public/assets/javascript/add.js
@@ -0,0 +1,13 @@
+// Handling new object when user clicks the "Add a Note" button
+
+// On click event
+$("#add-btn").on("click", function(event) {
+    event.preventDefault();
+
+    // Make a newNote object
+    let newNote = {
+        text: $("#text").val().trim(),
+        views: $("#views").val().trim(),
+        author_id: $("author_id").val().trim()
+    }
+})
