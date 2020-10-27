437c684861da387b2ff56b7c2ba8eeacfd418fb7
Sakiskid <tylerdrew.home@gmail.com>
2020-10-27 14:02:47 -0500

    note opens and closes on clicking the note or the X

--- public/assets/javascript/animations.js
+++ public/assets/javascript/animations.js
@@ -1,15 +1,28 @@
+// This is the actual animation. You can reference noteOpen to run animation methods
+const noteOpenAnim = anime({
+    targets: '#note-new-form',
+    translateY: -270,
+    opacity: 1,
+    autoplay: false,
+    begin: function(anim) {
+        isNoteOpenAnimCompleted = false;
+    },
+    complete: function(anim) {
+        isNoteOpenAnimCompleted = true;
+    },
+})
+// This prevents us from running the animation multiple times until it's finished
+let isNoteOpenAnimCompleted = true;
+
 function openNote() {
+    if (!isNoteOpenAnimCompleted) return; // If it's not completed, then return and do nothing
     console.log("Opening note... ");
-
-    let element = $("#note-new-form");
-
-    anime({
-        targets: '#note-new-form',
-        translateY: -270,
-        opacity: 1,
-    })
+    noteOpenAnim.direction = "normal";
+    noteOpenAnim.play();
 }
 
 function closeNote() {
-    
+    if (!isNoteOpenAnimCompleted) return; // If it's not completed, then return and do nothing
+    noteOpenAnim.direction = "reverse";
+    noteOpenAnim.play();
 }
