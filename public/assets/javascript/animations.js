e7cdcb76e20427b8de0ff6c8cc2d6e50e4886941
Sakiskid <tylerdrew.home@gmail.com>
2020-10-30 08:11:29 -0500

    refactored variables to make changing animation more modular

--- public/assets/javascript/animations.js
+++ public/assets/javascript/animations.js
@@ -40,39 +40,46 @@ function closeModalAnim() {
 
 // This is what makes the notes look really good as they load onto the page
 function initialNoteRenderAnim() {
+    // We put these variables at the top so they are easier to change if we want to modify the animation
+    const animDuration = 2500;
+    const staggerInMs = 100;
+    const rotationAmount = 25;
+    const flutterAmount = 20;
+
     $('.wallnote img').css("opacity", 0);
 
     // Fade In
     anime({
         targets: '.wallnote img',
         opacity: 1,
-        duration: 2500,
+        duration: animDuration,
         easing: 'linear',
-        delay: anime.stagger(100),
+        delay: anime.stagger(staggerInMs),
     });
 
     // Change Size as if falling in
     anime({
         targets: '.wallnote img',
         rotate: function () {
-            return anime.random(-25, 25);
+            return anime.random(-rotationAmount, rotationAmount);
         },
         scale: [2, 1],
         easing: 'linear',
-        duration: 2500,
-        delay: anime.stagger(100),
+        duration: animDuration,
+        delay: anime.stagger(staggerInMs),
     });
 
+    // Change position as if paper is fluttering onto the page
     anime({
         targets: '.wallnote img',
         left: () => {
-            return anime.random(-20, 20);
+            return anime.random(-flutterAmount, flutterAmount);
         },
         up: () => {
-            return anime.random(-20, 20);
+            return anime.random(-flutterAmount, flutterAmount);
         },
         easing: 'cubicBezier(0.465, 1.275, 0.550, -0.240)',
-        duration: 2500,
-        delay: anime.stagger(100),
+        duration: animDuration,
+        delay: anime.stagger(staggerInMs),
     })
 }
