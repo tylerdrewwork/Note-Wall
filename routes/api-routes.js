8f03c98263037bf6bec36ff8bfdbc013c7c4aade
JCJ <jesse.jackson.c@gmail.com>
2020-11-03 03:12:58 -0600

    removing unused commented code within api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -55,22 +55,6 @@ module.exports = function(app) {
     })
   });
 
-  // PUT route // Clueless as to the syntax... and about 6 askBCS chats didn't help either *facepalm*
-
-// app.put("/api/notes/views/:id", function (req, res) {
-//   db.Note.update(
-//     {id: req.body.id},
-//     {views: views +1
-//   }).then(function(dbNotes) {
-//     res.json(dbNotes);
-//   }).catch(function(dbError){
-//     console.log("XXX------HIT ERROR IN PUT ROUTE: \n", dbError)
-//   })
-// });
-
-// The below code is the original attempt... I stuck this all over the place... even once inside the PUT
-// Note.increment('views', { by: 1, where: { id: 'note' } });
-
 }
 
 
